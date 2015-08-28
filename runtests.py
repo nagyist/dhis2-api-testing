e99164e3df4927f6d088e6f16e5700c8172525f9
Jason P. Pickering <jason.p.pickering@gmail.com>
2015-08-28 09:15:04 +0200

    Catch any errors to continue the tests.

--- runtests.py
+++ runtests.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from __future__ import print_function
-from subprocess import check_call
+from subprocess import check_call,CalledProcessError
 import os
 import glob
 
@@ -10,6 +10,9 @@ def getTestFiles():
     return glob.glob(current_path + '/*.yaml');
 
 def runTestFile(file):
-    check_call(["resttest.py", "https://apps.dhis2.org/dev", file])
+    try:
+        output = check_call(["resttest.py", "https://apps.dhis2.org/dev", file])
+    except CalledProcessError as e:
+        print(e.returncode)
 
 map(lambda file: runTestFile(file), getTestFiles())
