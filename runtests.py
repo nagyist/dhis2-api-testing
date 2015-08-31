b7d74c0d6adf69e5f739775e875e66677b30033a
Jason P. Pickering <jason.p.pickering@gmail.com>
2015-08-31 10:17:05 +0200

    Fail the build if any of the tests fail.

--- runtests.py
+++ runtests.py
@@ -1,20 +1,33 @@
 #!/usr/bin/env python
 
 from __future__ import print_function
-from subprocess import check_call,CalledProcessError
+from subprocess import check_output, CalledProcessError, STDOUT
 import os
 import glob
 
+
 def getTestFiles():
-    current_path = os.path.dirname(os.path.realpath(__file__));
-    return glob.glob(current_path + '/*.yaml');
+    current_path = os.path.dirname(os.path.realpath(__file__))
+    return glob.glob(current_path + '/*.yaml')
 
 
 def runTestFile(file):
+   check_output(["resttest.py", "https://apps.dhis2.org/dev", file], stderr=STDOUT)
 
-    try:
-        output = check_call(["resttest.py", "https://apps.dhis2.org/dev", file])
-    except CalledProcessError as e:
-        print(e.returncode)
 
-map(lambda file: runTestFile(file), getTestFiles())
+
+def main():
+    files = getTestFiles()
+    passes = True
+    for yaml in files:
+        try:
+            runTestFile(yaml)
+        except CalledProcessError,e:
+            print (e)
+            passes = False
+    return passes
+
+if __name__ == "__main__":
+    success = main()
+    if not success:
+        raise Exception
